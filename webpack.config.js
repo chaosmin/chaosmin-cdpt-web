58c6f7d2794c207ddbd448f12d5e03751278d28a
Romani <chaosmin@hotmail.com>
2020-12-29 13:54:59 +0800

    添加gzip压缩

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CompressWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = {
   optimization: {
     minimizer: [new UglifyJsPlugin()]
-  }
+  },
+  plugins: [
+    new CompressWebpackPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: new RegExp('\\.(js|css)$'),
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
 }
